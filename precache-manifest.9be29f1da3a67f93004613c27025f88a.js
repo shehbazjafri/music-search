42dff78ccbf41829f8a06790af0dba191a5c00b4
shehbazjafri <galaxytwirls@gmail.com>
2019-10-16 22:39:23 +0530

    Updates

---
+++ precache-manifest.9be29f1da3a67f93004613c27025f88a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7a5aed9a5bf63eee548ee9b4afd1f15",
+    "url": "/music-search/index.html"
+  },
+  {
+    "revision": "32d4fdbdf61ce9aeda39",
+    "url": "/music-search/static/css/main.bfbcd896.chunk.css"
+  },
+  {
+    "revision": "af1e49f7348001cccdf4",
+    "url": "/music-search/static/js/2.fd223356.chunk.js"
+  },
+  {
+    "revision": "32d4fdbdf61ce9aeda39",
+    "url": "/music-search/static/js/main.a264e470.chunk.js"
+  },
+  {
+    "revision": "64afcbf1ebaaaf639e82",
+    "url": "/music-search/static/js/runtime-main.076e8a90.js"
+  }
+]);
