7f2e7e36ba9bfd9c52ef2854aa52c2839e41245f
shehbazjafri <galaxytwirls@gmail.com>
2019-10-16 22:28:36 +0530

    Init music search

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const url = "https://itunes.apple.com/search?term=";
+  const [results, setResults] = useState();
+  const [searchTerm, setSearchTerm] = useState("");
+
+  async function fetchData(searchTerm) {
+    try {
+      const response = await fetch(url + searchTerm, {
+        crossDomain: true,
+        method: "GET",
+        headers: { "Content-Type": "application/json" }
+      });
+      const data = await response.json();
+      setResults(data.results);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  const handleChange = event => {
+    setSearchTerm(event.target.value);
+  };
+
+  const searchArtist = event => {
+    event.preventDefault();
+    fetchData(searchTerm);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="search-box">
+        <form onSubmit={searchArtist}>
+          <input
+            type="text"
+            placeholder="Search artists"
+            onChange={handleChange}
+          />
+          <input className="search-button" type="submit" value="Search" />
+        </form>
+      </div>
+      {results && (
+        <div className="results">
+          {results.map((track, i) => {
+            return (
+              <div className="track" id={i} key={i}>
+                <img src={track.artworkUrl60} alt="track artwork" />
+                <h4>{track.trackName}</h4>
+              </div>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 }
